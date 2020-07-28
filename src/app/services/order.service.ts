41ac2075c83b1ac220cf6baca3c17fb22f50a5b1
hayarafee <hayaaaarafee@gmail.com>
2020-07-28 11:49:17 +0300

    first commit

---
+++ src/app/services/order.service.ts
@@ -0,0 +1,17 @@
+import { AuthHttp } from 'angular2-jwt';
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class OrderService {
+
+  constructor(private authHttp: AuthHttp ,private http: Http) {
+  }
+
+  getOrders() { 
+    
+    return this.authHttp.get('/api/orders')
+      .map(response => response.json());
+  }
+}
